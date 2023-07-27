a660b2a3ac345347791eecfa3ede44c74a66f92d
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-27 02:20:11 -0400

    Add mouseover event listener to each div

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const container = document.querySelector(".container");
-const containerWidth = container.offsetWidth;
-const sideOfDivs = 100;
+const containerWidth = container.offsetWidth - 2;
+const sideOfDivs = 16;
 const divSideWidth = containerWidth / sideOfDivs;
 
 for (let i = 0; i < sideOfDivs; ++i) {
   for (let j = 0; j < sideOfDivs; ++j) {
     const div = document.createElement("div");
+    div.style.border = "1px solid black";
     div.style.width = `${divSideWidth}px`;
     div.style.height = `${divSideWidth}px`;
     div.style.background = "crimson";
+    div.addEventListener("mouseover", function (e) {
+      // e.target.style.background = "blueviolet";
+      this.style.background = "blueviolet";
+    });
     container.append(div);
   }
 }
