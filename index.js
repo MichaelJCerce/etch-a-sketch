c00dd3bea70b108af04a81d1dba0ea201c05b42b
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-27 02:55:16 -0400

    Add button to let user specify dimensions of grid

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 const container = document.querySelector(".container");
 const containerWidth = container.offsetWidth - 2;
-const sideOfDivs = 16;
-const divSideWidth = containerWidth / sideOfDivs;
+const dimensionsButton = document.querySelector("button");
+let sideOfDivs = 16;
+let divSideWidth = containerWidth / sideOfDivs;
 
-for (let i = 0; i < sideOfDivs; ++i) {
-  for (let j = 0; j < sideOfDivs; ++j) {
-    const div = document.createElement("div");
-    div.style.border = "1px solid black";
-    div.style.width = `${divSideWidth}px`;
-    div.style.height = `${divSideWidth}px`;
-    div.style.background = "crimson";
-    div.addEventListener("mouseover", function (e) {
-      // e.target.style.background = "blueviolet";
-      this.style.background = "blueviolet";
-    });
-    container.append(div);
+dimensionsButton.addEventListener("click", () => {
+  const newDimension = +prompt(
+    "How many divs do you want per row/column: (num < 100) "
+  );
+  if (newDimension < 1 || newDimension > 100 || Number.isNaN(newDimension)) {
+    alert("Number must be less than or equal to 100");
+    return;
+  }
+  clearBoard();
+  sideOfDivs = newDimension;
+  divSideWidth = containerWidth / sideOfDivs;
+  drawBoard();
+});
+
+function drawBoard() {
+  for (let i = 0; i < sideOfDivs; ++i) {
+    for (let j = 0; j < sideOfDivs; ++j) {
+      const div = document.createElement("div");
+      div.style.border = "1px solid black";
+      div.style.width = `${divSideWidth}px`;
+      div.style.height = `${divSideWidth}px`;
+      div.style.background = "crimson";
+      div.addEventListener("mouseover", function (e) {
+        // e.target.style.background = "blueviolet";
+        this.style.background = "blueviolet";
+      });
+      container.append(div);
+    }
   }
 }
+
+function clearBoard() {
+  for (let i = 0; i < sideOfDivs; ++i) {
+    for (let j = 0; j < sideOfDivs; ++j) {
+      container.removeChild(container.lastChild);
+    }
+  }
+}
+
+drawBoard();
