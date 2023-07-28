9367d697a3fb38f02be3cec1bfeef37c6aaca83d
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-27 20:54:34 -0400

    Removed the height/width of each cell
    Realized the solution wasn't taking advantage of what makes flexbox
    so helpful.  Manually calculating the width and height was casuing
    a lot of problems.

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const grid = document.querySelector(".grid");
-const dimensionButton = document.querySelector("button");
-const mediumQuery = window.matchMedia("(min-width: 500px)");
-const largeQuery = window.matchMedia("(min-width: 1000px)");
+const changeDimensionsButton = document.querySelector("button");
 
-let gridWidth =
-  grid.offsetWidth -
-  2 * parseFloat(window.getComputedStyle(grid).paddingTop) -
-  2 * parseFloat(window.getComputedStyle(grid).borderTopWidth);
-let gridDimension = 16;
-let divDimension = gridWidth / gridDimension;
 
-function drawBoard() {
-  for (let i = 0; i < gridDimension; ++i) {
+
+function drawBoard(divsPerSide) {
+  for (let i = 0; i < divsPerSide; ++i) {
     const row = document.createElement("div");
+
     row.classList.add("row");
-    for (let j = 0; j < gridDimension; ++j) {
+    
+    for (let j = 0; j < divsPerSide; ++j) {
       const column = document.createElement("div");
+
       column.classList.add("column");
-      column.style.width = `${divDimension}px`;
-      column.style.height = `${divDimension}px`;
       column.addEventListener("mouseover", function () {
         // this.style.background = "blueviolet";
         this.style.background = generateRandomColor();
       });
+
       row.append(column);
     }
+
     grid.append(row);
   }
 }
@@ -35,39 +31,24 @@ function generateRandomColor() {
   )}, ${Math.floor(Math.random() * 256)})`;
 }
 
-function updateGridSize() {
-  gridWidth =
-    grid.offsetWidth -
-    2 * parseFloat(window.getComputedStyle(grid).paddingTop) -
-    2 * parseFloat(window.getComputedStyle(grid).borderTopWidth);
-  divDimension = gridWidth / gridDimension;
-
-  for (let i = 0; i < gridDimension; ++i) {
-    const row = grid.childNodes[i];
-    console.dir(grid.childNodes);
-    for (let j = 0; j < gridDimension; ++j) {
-      row.childNodes[j].style.width = `${divDimension}px`;
-      row.childNodes[j].style.height = `${divDimension}px`;
-    }
-  }
-}
-
-function updateGridDimensions() {
-  const newDimension = +prompt(
+function updateDivsPerSide() {
+  const newDivDimension = +prompt(
     "How many divs do you want per row/column: (num <= 100) "
   );
-  if (newDimension < 1 || newDimension > 100 || Number.isNaN(newDimension)) {
+
+  if (
+    newDivDimension < 1 ||
+    newDivDimension > 100 ||
+    Number.isNaN(newDivDimension)
+  ) {
     alert("Number must be less than or equal to 100");
     return;
   }
+
   grid.innerHTML = "";
-  gridDimension = newDimension;
-  divDimension = gridWidth / gridDimension;
-  drawBoard();
+  drawBoard(newDivDimension);
 }
 
-mediumQuery.addEventListener("change", updateGridSize);
-largeQuery.addEventListener("change", updateGridSize);
-dimensionButton.addEventListener("click", updateGridDimensions);
+changeDimensionsButton.addEventListener("click", updateDivsPerSide);
 
-drawBoard();
+drawBoard(16);
